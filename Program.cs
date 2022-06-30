c3873f2dbb95f6898b90d2e6f988c2010f3a9dc9
Cainã <105826739+livecaina@users.noreply.github.com>
2022-06-30 16:42:56 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,6 +9,16 @@ namespace provaex5
     internal class Program
     {
         static void Main(string[] args) {
+            int n, p; 
+      
+            for(n = 0; n <= 50 ;n++) 
+            {
+                Console.WriteLine(n*2);
+                Console.ReadKey();
+            }
+
+            Console.ReadKey();
         }
+       
     }
 }
